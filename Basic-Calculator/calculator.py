1c30cfdfbef4a45d6f8b5d45fc8e89f67d036e4c
Sachinbhujel <90704616+Sachinbhujel@users.noreply.github.com>
2022-01-17 21:44:17 +0530

    Add files via upload

---
+++ Basic-Calculator/calculator.py
@@ -0,0 +1,20 @@
+print("This is a basic calculator")
+firstNumber = int(input("enter your first number :"))
+secondNumber = int(input("Enter your second number : "))
+operator = input("Enter your operation : ")
+	
+if operator == "+":
+	print("Answer is" , firstNumber+secondNumber)
+elif operator == "-":
+	print("Answer is" ,firstNumber-secondNumber)
+elif operator == "/":
+	print( "Answer is" ,firstNumber/secondNumber)
+elif operator == "*":
+	print(" Answer is" , firstNumber*secondNumber)
+	
+a=1
+print(a)
+print(f"{a}")
+	
+	
+	
